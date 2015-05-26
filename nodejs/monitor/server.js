d7555267236119238325947fb2596e8108808957
Niels Koster <n.koster@portavita.eu>
2015-05-26 23:19:28 +0200

    Initial

---
+++ nodejs/monitor/server.js
@@ -0,0 +1,48 @@
+var net = require('net');
+
+var WebSocketServer = require('./').Server
+  , http = require('http')
+  , express = require('express')
+  , app = express.createServer();
+
+app.use(express.static(__dirname + '/public'));
+app.listen(8080);
+
+var dataToSend = "";
+
+var wss = new WebSocketServer({server: app});
+wss.on('connection', function(ws) {
+  var id = setInterval(function() {
+    if (dataToSend.length != "") {
+      ws.send(dataToSend, function() { /* ignore errors */ });
+      console.log('DATA changed and send');
+      dataToSend = "";
+    }
+  }, 100);
+  ws.on('close', function() {
+    console.log('Client Closed');
+    clearInterval(id);
+  });
+});
+
+var HOST = '127.0.0.1';
+var PORT = 6969;
+
+net.createServer(function(sock) {
+    
+    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+    sock.on('data', function(data) {
+        dataToSend = data;
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        var dataString = "" + data;
+        dataToSend = dataString.replace(/(\r\n|\n|\r)/, '');
+        sock.write('You said "' + dataToSend + '"' + "\n");
+    });
+    
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+    });
+    
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
