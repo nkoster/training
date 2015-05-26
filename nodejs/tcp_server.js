71d51cdf8d726f13eccb1ebf1747cdd49b025faf
Niels Koster <n.koster@portavita.eu>
2015-05-26 21:19:55 +0200

    Initial

---
+++ nodejs/tcp_server.js
@@ -0,0 +1,23 @@
+var net = require('net');
+
+var HOST = '127.0.0.1';
+var PORT = 6969;
+
+net.createServer(function(sock) {
+    
+    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+    sock.on('data', function(data) {
+        
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        var dataString = "" + data;
+        sock.write('You said "' + dataString.replace(/(\r\n|\n|\r)/, '') + '"');
+        
+    });
+    
+    sock.on('close', function(data) {
+        console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+    });
+    
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
