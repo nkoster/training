85c42e709582401982ded8cf06de97c326846d8b
Niels Koster <n.koster@portavita.eu>
2015-06-02 00:40:46 +0200

    Big improvements

--- nodejs/monitor/server.js
+++ nodejs/monitor/server.js
@@ -8,22 +8,14 @@ var WebSocketServer = require('./').Server
 app.use(express.static(__dirname + '/public'));
 app.listen(8080);
 
-var dataToSend = "";
-
 var log = false;
 
 var wss = new WebSocketServer({server: app});
+
 wss.on('connection', function(ws) {
-  var id = setInterval(function() {
-    if (dataToSend.length != "") {
-      ws.send(dataToSend, function() { /* ignore errors */ });
-      if (log) console.log('DATA changed and sent');
-      dataToSend = "";
-    }
-  }, 100);
+  if (log) console.log('DATA changed and sent');
   ws.on('close', function() {
     if (log) console.log('Client Closed');
-    clearInterval(id);
   });
 });
 
@@ -31,19 +23,19 @@ var HOST = '127.0.0.1';
 var PORT = 6969;
 
 net.createServer(function(sock) {
-    
-    if (log) console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
-    sock.on('data', function(data) {
-        var dataString = "" + data;
-        dataToSend = dataString.replace(/(\r\n|\n|\r)/, '');
-        if (log) console.log('DATA ' + sock.remoteAddress + ': ' + dataToSend);
-        sock.write('You said "' + dataToSend + '"' + "\n");
-    });
-    
-    sock.on('close', function(data) {
-        if (log) console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
-    });
-    
+  if (log) console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+  sock.on('data', function(data) {
+    var dataToSend = '' + data;
+    dataToSend = dataToSend.replace(/(\r\n|\n|\r)/, '');
+    wss.clients.forEach(function (conn) {
+      conn.send(dataToSend, function() { /* no error handling */ });
+    })
+    if (log) console.log('DATA ' + sock.remoteAddress + ': ' + data);
+    if (log) sock.write('You said "' + data + '"' + "\n");
+  });
+  sock.on('close', function(data) {
+    if (log) console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+  });
 }).listen(PORT, HOST);
 
 console.log('Server listening on ' + HOST +':'+ PORT);
